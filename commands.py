f3c7ea6618d9efca026b6884358c38b2f33943e3
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2019-11-10 19:38:41 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -11,16 +11,34 @@ while True:
             p = ParkingLot(int(s[1]))
             p.createSlots(int(s[1]))
         elif s[0] == 'park':
-            p.addCar(s[1] ,s[2])
+            try:
+                p.addCar(s[1] ,s[2])
+            except:
+                print('First create a Parking Lot')
         elif s[0] == 'leave':
-            p.leaveCar(int(s[1]))
+            try:
+                p.leaveCar(int(s[1]))
+            except:
+                print('First create a Parking Lot')
         elif s[0] == 'status':
-            p.status()
+            try:
+                p.status()
+            except:
+                print('First create a Parking Lot')
         elif s[0] == 'registration_numbers_for_cars_with_colour':
-            p.getColorRegestration(s[1])
+            try:
+                p.getColorRegestration(s[1])
+            except:
+                print('First create a Parking Lot')
         elif s[0] == 'slot_numbers_for_cars_with_colour':
-            p.getColorSlots(s[1])
+            try:
+                p.getColorSlots(s[1])
+            except:
+                print('First create a Parking Lot')
         elif s[0] == 'slot_number_for_registration_number':
-            p.slotNumber(s[1])
+            try:
+                p.slotNumber(s[1])
+            except:
+                print('First create a Parking Lot')
     except:
         break
