35653bb3d0bcafaa3a01e3c379b3c44de683c260
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2019-11-10 10:40:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,14 +23,21 @@ class ParkingLot:
         if self.occupied_no == 0:
             print('Sorry, parking lot is full')
     def leaveCarByRegestration(self, regestration):           #Function for car leave from a slot and can be used to show its regestration number
+        check = 0
         for key in self.__slots: 
             if self.__slots[key] == regestration_no:
                 print('Slot number '+str(key)+' '+'is free')
                 self.__slots[key] = [0,0]
+                check += 1
                 break
+        if check == 0:
+            print('No Car of this regestration number')
     def leaveCar(self,slotNumber):                    #Function for slotNumber from which the car is leaving
-        self.__slots[slotNumber] = [0,0] 
-        print('Slot number '+str(slotNumber)+' '+'is free')
+        if self.__slots[slotNumber] != [0,0]:
+            self.__slots[slotNumber] = [0,0] 
+            print('Slot number '+str(slotNumber)+' '+'is free')
+        else:
+            print('The slot is already empty')
     def status(self):                                 #Status of the parking lot
         print('Slot No.'+'   '+'Regestration No'+'   '+'Color')
         for i in range(1,self.n+1):
@@ -39,7 +46,10 @@ class ParkingLot:
             except:
                 print('parking lot is empty')
     def slotsColor(self,color):          #Function showing the regestration numbers of a particular number
-        print(','.join(self.__colors[color])) 
+        if len(self.__colors[color]) != 0:
+            print(','.join(self.__colors[color])) 
+        else:
+            print('There is no car of this particular color')
     def slotNumber(self,regestration_no):        #Function showing the slot number of a particular car
         p = 0 
         for key in self.__slots:
@@ -50,12 +60,20 @@ class ParkingLot:
         if p==0:
             print('Not Found')
     def getColorSlots(self,color):            #Function showing slot numbers of a particular color
+        check = 0
         for key in self.__slots:
             if (self.__slots[key])[1] == color:
                 print(str(key),end=', ')
+                check += 1
+        if check == 0:
+            print('No slot occupied by any car of this color')
         print('')
     def getColorRegestration(self,color):     #Function showing regestration numbers of a particular color
+        check = 0
         for key in self.__slots:
             if (self.__slots[key])[1] == color:
                 print(str((self.__slots[key])[0]),end= ', ')
+                check += 1
+        if check == 0:
+            print('No slot occupied by any car of this color')
         print('')
